db3414ca50862606afb6d3170a4a2152de355fc6
Luciano Moser <lucianomoser01@gmail.com>
2018-03-05 21:29:37 -0300

    Começando a implementar um metodo que imprime o tabuleiro

--- xadrez-console-moser/Program.cs
+++ xadrez-console-moser/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
-namespace xadrez_console_moser
+namespace tabuleiro
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Tabuleiro.Tabuleiro tab = new Tabuleiro.Tabuleiro(8,8);
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
 
